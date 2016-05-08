c74d592bc4d74ccf0a6851554ba5356edc9c0c12
Kevin <keam.crf@gmail.com>
2016-05-07 20:30:44 -0500

    Creacion de clase consultas y sus metodos (aun no funcionales)

---
+++ GYM/GYM/Consultas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GYM
+{
+    class Consultas
+    {
+        private void inserta()
+        {
+
+        }
+
+        private void Selecciona()
+        {
+
+        }
+
+        private void Elimina()
+        {
+
+        }
+
+        private void Modifica()
+        {
+
+        }
+    }
+}
+
