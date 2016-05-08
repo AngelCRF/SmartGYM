acd72069bcfecccbdf3344d7c0bd0c54b48d0a8c
Carlos <Carlos@SPUNKY>
2016-05-08 00:00:35 -0500

    comit

--- GYM/GYM/Form1.Designer.cs
+++ GYM/GYM/Form1.Designer.cs
@@ -97,6 +97,7 @@
             // 
             this.textBox_password.Location = new System.Drawing.Point(111, 207);
             this.textBox_password.Name = "textBox_password";
+            this.textBox_password.PasswordChar = '•';
             this.textBox_password.Size = new System.Drawing.Size(141, 20);
             this.textBox_password.TabIndex = 5;
             // 
